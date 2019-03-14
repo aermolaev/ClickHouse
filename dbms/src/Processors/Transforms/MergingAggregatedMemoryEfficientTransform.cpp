f2daad4c214e2e632e458322039db6b4ccaab150
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2019-03-14 18:22:57 +0300

    Fix build.

--- dbms/src/Processors/Transforms/MergingAggregatedMemoryEfficientTransform.cpp
+++ dbms/src/Processors/Transforms/MergingAggregatedMemoryEfficientTransform.cpp
@@ -317,7 +317,7 @@ void MergingAggregatedBucketTransform::transform(Chunk & chunk)
         block.info.is_overflows = agg_info->is_overflows;
         block.info.bucket_num = agg_info->bucket_num;
 
-        blocks_list.emplace_back();
+        blocks_list.emplace_back(block);
     }
 
     chunk.setChunkInfo(nullptr);
